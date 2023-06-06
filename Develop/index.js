7be974c4a56d35fd8d2d2c10eb78a3064b82c100
konawave <nickkjeffs@gmail.com>
2023-06-06 11:53:14 -0400

    Added more files to Develop folder

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,73 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const path = require("path");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  {
+    type: "input",
+    message: "WWhat is the title of the project?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "What is this project's description?",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What are the install instructions?",
+    name: "install",
+    default: "npm i"
+  },
+  {
+    type: "input",
+    message: "What is this project used for?",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "What is your Table of Contents?",
+    name: "table of contents"
+  },
+  {
+    type: "input",
+    message: "What commands should be run to run tests?",
+    name: "tests",
+    default: "npm run test"
+  },
+  {
+    type: "input",
+    message: "Who needs credit on this project?",
+    name: "credits"
+  },
+  {
+    type: "input",
+    message: "How does someone contribute to this project?",
+    name: "contribute"
+  },
+  {
+    type: "list",
+    message: "How is this project licensed?",
+    name: "license",
+    choices: ["Apache", "Boost Software", "Creative Commons", "GNU", "ISC", "MIT", "Mozilla Public", "The Unlicense", "Other"]
+  }
+]
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to write README file
+function writeToFile(filename, data) {
+  return fs.writeFileSync(path.join(process.cwd(), filename), data);
+}
 
-// Function call to initialize app
+// function to initialize program
+function init() {
+  inquirer.prompt(questions).then(response => {
+    writeToFile("sampleREADME.md", generateMarkdown(response))
+  });
+}
+
+// function call to initialize program
 init();
